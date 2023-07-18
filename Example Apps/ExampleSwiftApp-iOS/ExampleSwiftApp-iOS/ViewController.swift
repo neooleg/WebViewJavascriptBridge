d2324e7c271a2e5fd39e70f62e870831aad3efe2
Oleg V <olegv.pro@gmail.com>
2023-07-18 10:39:32 +0200

    Cocoapods fixed:    - "pod install" made working on local machine (after installing the recommended version). It seems that's why a lot of files were ammended.
    Swift Example App (not tests) modified to show WebView:
      - It lets user click on a second half (by vertical) for running JS thru bridge (using reimplemented showAlert() js function).
      - Background colors were setting to indicate what's changed.
      - To make WebView dubbuggable in Safari -- iOS target set to 16.4 (see Apple's docs).

--- Example Apps/ExampleSwiftApp-iOS/ExampleSwiftApp-iOS/ViewController.swift
+++ Example Apps/ExampleSwiftApp-iOS/ExampleSwiftApp-iOS/ViewController.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import WebKit
+
+import WebViewJavascriptBridge
+
+func loadEchoSample(_ webView: Any) {
+    let request = URLRequest.init(url: Bundle.main.url(forResource: "echo", withExtension: "html")!)
+//    if webView is UIWebView {
+//        (webView as! UIWebView).loadRequest(request)
+//    } else {
+        (webView as! WKWebView).load(request)
+//    }
+}
 
 class ViewController: UIViewController {
+    
+    var webView: WKWebView = WKWebView.init()
+    var bridge: WebViewJavascriptBridge = WebViewJavascriptBridge.init();
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // --- WebView ---
+        var frame = view.bounds
+        frame.size.height /= 2
+        
+        webView = WKWebView.init(frame: frame)
+        webView.backgroundColor = UIColor.red
+        webView.isInspectable = true
+        
+        // --- WebView Bridge ---
+        loadEchoSample(webView)
+        bridge = WebViewJavascriptBridge.init(webView);
+        
+        view.addSubview(webView)
     }
+    
+    // --- [START: For debugging purposes] Taps support ---
+    @objc private func handleTapGesture(_ gesture: UITapGestureRecognizer) {
+        bridge.callHandler("showAlert", data: "Hi from ObjC to JS!")
+    }
+    // --- [END: For debugging purposes] Taps support ---
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
